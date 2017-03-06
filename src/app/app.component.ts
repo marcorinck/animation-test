8e44d4ea905f0c1602ded4dc6376465b29dac543
marcorinck <marco.rinck@gmail.com>
2017-03-06 10:02:40 +0100

    animations test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, trigger, state, style, transition, animate} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('background', [
+      state('red', style({
+        backgroundColor: '#dc3f36',
+        color: '#fff'
+      })),
+      state('green', style({
+        backgroundColor: '#20b671',
+        color: '#fff'
+      })),
+      state('blue', style({
+        backgroundColor: 'blue',
+        color: '#fff'
+      })),
+      state('white', style({
+        backgroundColor: '#fff',
+        color: '#7d7d7d'
+      })),
+      transition('void <=> *', animate('0ms')),
+      transition('* <=> *', animate('300ms ease-in')),
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
+
+  color = "white";
+
+  changeColor(color) {
+    this.color = color;
+  }
 }
